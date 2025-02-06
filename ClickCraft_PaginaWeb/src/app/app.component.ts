44e891cb13c3ec84148e095bbf64e1d97cb46db0
Neo Armada <Neoarmadam@gmail.com>
2025-02-06 23:02:21 +0100

    Mondongo "Funcionando"

--- ClickCraft_PaginaWeb/src/app/app.component.ts
+++ ClickCraft_PaginaWeb/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { RouterOutlet } from '@angular/router';
+import { MondongoComponent } from './mondongo/mondongo.component';
 
 @Component({
   selector: 'app-root',
-  imports: [FooterComponent, HeaderComponent, RouterOutlet],
+  imports: [FooterComponent, HeaderComponent, RouterOutlet, MondongoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
