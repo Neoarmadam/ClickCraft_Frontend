1237bfc3221f7d4b2510a344d6913b0ecb693bed
Neo Armada <Neoarmadam@gmail.com>
2025-02-06 22:13:40 +0100

    acercade creado
    Componente acercade creado, funciona su ruta pero falta cambiar el contenido para que se adecue al trabajo

--- ClickCraft_PaginaWeb/src/app/app.component.ts
+++ ClickCraft_PaginaWeb/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [FooterComponent, HeaderComponent],
+  imports: [FooterComponent, HeaderComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- ClickCraft_PaginaWeb/src/app/app.routes.ts
+++ ClickCraft_PaginaWeb/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { AcercadeComponent } from './acercade/acercade.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'acercade',
+        component: AcercadeComponent,
+    }
+];
