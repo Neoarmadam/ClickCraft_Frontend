2e58300b24e10c50f26779b4de067986cdfd41fd
Neo Armada <Neoarmadam@gmail.com>
2025-02-06 22:28:52 +0100

    Anaconda creada
    Juego del snake creado y finalizado.

--- ClickCraft_PaginaWeb/src/app/app.routes.ts
+++ ClickCraft_PaginaWeb/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { AcercadeComponent } from './acercade/acercade.component';
+import { SnakeGameComponent } from './snake-game/snake-game.component';
 
 export const routes: Routes = [
     {
         path: 'acercade',
         component: AcercadeComponent,
+    },
+    {
+        path: 'SnakeGame',
+        component: SnakeGameComponent,
     }
 ];
