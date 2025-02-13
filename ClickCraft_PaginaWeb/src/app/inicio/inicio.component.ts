819fa0e32ccc8855d8646da2ee5409560aeb62ca
Neo Armada <Neoarmadam@gmail.com>
2025-02-13 19:27:40 +0100

    Variedad de Ratones
    Por array se ha agregado mas variedad de ratones, para hacer la pagina mas vistosa

--- ClickCraft_PaginaWeb/src/app/inicio/inicio.component.ts
+++ ClickCraft_PaginaWeb/src/app/inicio/inicio.component.ts
@@ -7,5 +7,47 @@ import { Component } from '@angular/core';
   styleUrl: './inicio.component.css'
 })
 export class InicioComponent {
-  animes = new Array(15);
+  ratones = [{
+    codigo: 1,
+    nombre: "Raton Generico",
+    descripcion: "Raton generico"
+  },{
+    codigo: 2,
+    nombre: "Raton RGB Azul",
+    descripcion: "Raton con luces azules"
+  },{
+    codigo: 3,
+    nombre: "Raton Rayitos",
+    descripcion: "Raton con luces de rayitos"
+  },{
+    codigo: 4,
+    nombre: "Raton Generico",
+    descripcion: "Raton generico"
+  },{
+    codigo: 5,
+    nombre: "Raton Logitec",
+    descripcion: "Raton de marca Logitec"
+  },{
+    codigo: 6,
+    nombre: "Raton Telefono",
+    descripcion: "Raton para hacer llamadas"
+  },{
+    codigo: 7,
+    nombre: "Raton Gamer Malo",
+    descripcion: "Raton generico con luces"
+  },{
+    codigo: 8,
+    nombre: "Raton Rayitos++",
+    descripcion: "Raton Rayitos Blanco"
+  },{
+    codigo: 9,
+    nombre: "Raton TT",
+    descripcion: "Raton con carita llorando"
+  },{
+    codigo: 10,
+    nombre: "Raton Rojo LGB",
+    descripcion: "Raton que usaria un Sitd"
+  }
+
+  ];
 }
